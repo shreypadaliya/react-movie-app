a54681df5873007c991bc91152c3f5f4fc8ecbff
shreypadaliya <shreypadaliya@gmail.com>
2024-06-25 15:25:27 +0530

    update loader

--- src/Movies.jsx
+++ src/Movies.jsx
@@ -1,17 +1,30 @@
 import React, { useContext } from "react";
 import { AppContext } from "./context";
 import { NavLink } from "react-router-dom";
+import { MagnifyingGlass } from "react-loader-spinner";
 
 const Movies = () => {
   const { movie, isLoading } = useContext(AppContext);
   // console.log(movie);
 
-  if(isLoading){
+  if (isLoading) {
     return (
       <div>
-        <div className='loading'>Loading...</div>
+        <div className="loading">
+          {" "}
+          <MagnifyingGlass
+            visible={true}
+            height="80"
+            width="80"
+            ariaLabel="magnifying-glass-loading"
+            wrapperStyle={{}}
+            wrapperClass="magnifying-glass-wrapper"
+            glassColor="#c0efff"
+            color="#e15b64"
+          />
+        </div>
       </div>
-    )
+    );
   }
 
   return (
@@ -24,19 +37,23 @@ const Movies = () => {
                <div>
               <h2>{curMovie.Title}</h2>
             </div> 
-            }; */ 
+            }; */
             }
-              const {imdbID,Title,Poster} = curMovie;
-              const movieName = Title.substring(0,15);
+            const { imdbID, Title, Poster } = curMovie;
+            const movieName = Title.substring(0, 15);
 
-            return <NavLink to={`movie/${imdbID}`}>
-              <div className="card">
-                <div className="card-info">
-                  <h2>{movieName.length > 15 ? `{movieName}...` : movieName}</h2>
-                  <img src={Poster} alt="" />
+            return (
+              <NavLink to={`movie/${imdbID}`}>
+                <div className="card">
+                  <div className="card-info">
+                    <h2>
+                      {movieName.length > 15 ? `{movieName}...` : movieName}
+                    </h2>
+                    <img src={Poster} alt="" />
+                  </div>
                 </div>
-              </div>
-            </NavLink>
+              </NavLink>
+            );
           })}
         </div>
       </section>
--- src/SingleMovie.jsx
+++ src/SingleMovie.jsx
@@ -1,64 +1,74 @@
-import { NavLink, useParams } from 'react-router-dom'
-import { API_URL } from './context';
-import { useState,useEffect } from 'react';
+import { NavLink, useParams } from "react-router-dom";
+import { API_URL } from "./context";
+import { useState, useEffect } from "react";
+import { ThreeDots } from "react-loader-spinner";
 
 const SingleMovie = () => {
-  const {id} = useParams();
-  
-const [isLoading,setIsLoading] = useState(true);
-const [movie,setMovie] = useState("")
+  const { id } = useParams();
 
-const getMovies = async(url) => {
-  setIsLoading(true);
-     try{
-          const res = await fetch(url);
-          const data = await res.json()
-          // console.log(data)
-          if(data?.Response === "True"){
-               setIsLoading(false);
-               setMovie(data);
-          }
-     } 
-     catch(error){
-          console.log(error)
-     }
-} 
-  
-useEffect(() => {
-     let timerOut = setTimeout(()=> {getMovies(`${API_URL}&i=${id}`);},1000);     
-     return () => clearTimeout(timerOut)  
-}, [id]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [movie, setMovie] = useState("");
 
-  if(isLoading){
+  const getMovies = async (url) => {
+    setIsLoading(true);
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      // console.log(data)
+      if (data?.Response === "True") {
+        setIsLoading(false);
+        setMovie(data);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    let timerOut = setTimeout(() => {
+      getMovies(`${API_URL}&i=${id}`);
+    }, 1000);
+    return () => clearTimeout(timerOut);
+  }, [id]);
+
+  if (isLoading) {
     return (
-      <div className='movie-section'>
-        <div className='loading'>Loading...</div>
+      <div className="movie-section">
+        <div className="loading">
+          <ThreeDots
+            visible={true}
+            height="80"
+            width="80"
+            color="white"
+            radius="9"
+            ariaLabel="three-dots-loading"
+            wrapperStyle={{}}
+            wrapperClass=""
+          />
+        </div>
       </div>
-    )
+    );
   }
 
   return (
-    
-      <section className='movie-section'>
-        <div className='movie-card'>
-          <figure>
-            <img src={movie.Poster} alt="" />
-          </figure>
-          <div className='card-content'>
-            <p className='title'>{movie.Title}</p>
-            <p className='card-text'>{movie.Released}</p>
-            <p className='card-text'>{movie.Genre}</p>
-            <p className='card-text'>{movie.imdbRating}</p>
-            <p className='card-text'>{movie.Country}</p>
-            <NavLink to="/" className='back-btn'>Go Back</NavLink>
-          </div>
+    <section className="movie-section">
+      <div className="movie-card">
+        <figure>
+          <img src={movie.Poster} alt="" />
+        </figure>
+        <div className="card-content">
+          <p className="title">{movie.Title}</p>
+          <p className="card-text">{movie.Released}</p>
+          <p className="card-text">{movie.Genre}</p>
+          <p className="card-text">{movie.imdbRating}</p>
+          <p className="card-text">{movie.Country}</p>
+          <NavLink to="/" className="back-btn">
+            Go Back
+          </NavLink>
         </div>
-      </section>
-    
+      </div>
+    </section>
+  );
+};
 
-  )
-
-  
-}
-
-export default SingleMovie
+export default SingleMovie;
